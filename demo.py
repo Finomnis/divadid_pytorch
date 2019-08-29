9c6e76c54139d57dddf7c1d514114fd56facc8f8
Finomnis <martin.h.stumpf@gmail.com>
2019-08-29 18:25:56 +0200

    added border handling on overlaps, added status outputs in demo

--- demo.py
+++ demo.py
@@ -5,35 +5,48 @@ from PIL import Image
 
 from src import GradientMap
 
-import torch
+import torch.cuda
+import sys
+
 
 # Enable CUDA
 dev = torch.device('cuda')
 print("Device:", dev)
+if dev.type == 'cuda':
+    print("Initializing CUDA ... ", end=''); sys.stdout.flush()
+    # Initializing CUDA shouldn't be done manually. Here, it is done for aesthetic reasons, as it would mess up
+    # the timing of the prints.
+    torch.Tensor([]).to(device=dev)
+    print("done")
 
 # Load Images
-bg = Image.open('img/bg_large.jpeg')
+print("Opening images ... ", end=''); sys.stdout.flush()
+bg = Image.open('img/bg.jpg')
 fg = Image.open('img/fg.jpg')
+print("done")
 
 # Compute Gradient Maps
+print("Computing gradients ... ", end=''); sys.stdout.flush()
 bg_grad = GradientMap.from_image(bg, device=dev)
 fg_grad = GradientMap.from_image(fg, device=dev)
+print("done")
 
 # Insert foreground gradient into background
-bg_grad.paste_gradient(fg_grad, 10, 10)
+print("Merging gradients ... ", end=''); sys.stdout.flush()
+bg_grad.paste_gradient(fg_grad, -20, -20)
+print("done")
 
 # COMPUTE
-print("Computing ...", end = '')
+print("Reconstruction ... ", end=''); sys.stdout.flush()
 start = time()
 
-num_iters = 100
+num_iters = 10000
 bg_grad.reconstruct(num_iters)
 img = bg_grad.get_image()
 
 duration = time() - start
 print("done")
 print("Speed:", num_iters/duration, "iter/s")
-# COMPUTE DONE
 
 # Show output image
 img.show()
--- src/GradientMap.py
+++ src/GradientMap.py
@@ -5,11 +5,12 @@ from torch.utils.cpp_extension import load
 import numpy as np
 
 import os
-import time
+import sys
 
 # Load external cuda file
 script_path = os.path.dirname(os.path.realpath(__file__))
-print("Loading compute kernels ... ", end = '')
+print("Loading compute kernels ... ", end='')
+sys.stdout.flush()
 ext_cuda = load(name='ext_cuda', sources=[
     os.path.join(script_path, 'cuda', 'GradientMap.cpp'),
     os.path.join(script_path, 'cuda', 'GradientMap_cuda.cu'),
@@ -47,23 +48,44 @@ class GradientMap:
     def paste_gradient(self, other, x, y, boost=1.0):
         """Inserts a given gradient at position x,y into self."""
 
-        # Not entirely sure if that implementation is exactly equivalent to the original, needs review
-        _, size_x, size_y = other.img.shape
+        # README: I am not entirely sure if the cuda implementation is exactly equivalent to the original, needs review
 
-        # Cut.
-        # TODO: Implement. Currently relies on the fact that the pasted image is completely inside our image
-        grad_self_x = self.grad_x[:, x:x + size_x, y:y + size_y]
-        grad_self_y = self.grad_y[:, x:x + size_x, y:y + size_y]
-        grad_other_x = other.grad_x
-        grad_other_y = other.grad_y
+        # Extract and pad
+        grad_other_x = torch.nn.functional.pad(other.grad_x, [0, 1, 0, 0])
+        grad_other_y = torch.nn.functional.pad(other.grad_y, [0, 0, 0, 1])
+        assert(grad_other_x.shape == grad_other_y.shape)
+
+        # Cut grad_other for the first time, if x < 0 or y < 0
+        if x < 0:
+            grad_other_x = grad_other_x[:, :, -x:]
+            grad_other_y = grad_other_y[:, :, -x:]
+            x = 0
+        if y < 0:
+            grad_other_x = grad_other_x[:, -y:, :]
+            grad_other_y = grad_other_y[:, -y:, :]
+            y = 0
+
+        # Cut grad_self to fit grad_other
+        grad_self_x = self.grad_x[:, y:y + grad_other_x.size(1), x:x + grad_other_x.size(2)]
+        grad_self_y = self.grad_y[:, y:y + grad_other_x.size(1), x:x + grad_other_x.size(2)]
+
+        # Cut grad_self with itself to ensure they are identical in size.
+        # Possible, because they are both missing one row or column.
+        grad_self_x = grad_self_x[:, :grad_self_y.size(1), :grad_self_y.size(2)]
+        grad_self_y = grad_self_y[:, :grad_self_x.size(1), :grad_self_x.size(2)]
+        assert(grad_self_x.shape == grad_self_y.shape)
+
+        # If grad_self is a different size than grad_other, this means we are on the right or bottom edge.
+        # Then, cut grad_other again, this time on the right and bottom side
+        grad_other_x = grad_other_x[:, :grad_self_y.size(1), :grad_self_x.size(2)]
+        grad_other_y = grad_other_y[:, :grad_self_y.size(1), :grad_self_x.size(2)]
+        assert (grad_other_x.shape == grad_other_y.shape)
+        assert (grad_other_x.shape == grad_self_x.shape)
+        assert (grad_other_y.shape == grad_self_y.shape)
 
         # Apply boosting
         grad_other_x *= boost
         grad_other_y *= boost
-
-        # Pad
-        grad_other_x = torch.nn.functional.pad(grad_other_x, (0, 1, 0, 0))
-        grad_other_y = torch.nn.functional.pad(grad_other_y, (0, 0, 0, 1))
 
         # Compute replacement condition
         grad_self_norm = grad_self_x ** 2 + grad_self_y ** 2
@@ -80,4 +102,3 @@ class GradientMap:
                 ext_cuda.step(i, self.img, self.grad_x)
             else:
                 ext_cuda.step(i, self.img, self.grad_y)
-
