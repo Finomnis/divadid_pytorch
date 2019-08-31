3f3a4c11800e5463088da2fbeaae06b5db1ebfa0
Finomnis <martin.h.stumpf@gmail.com>
2019-08-31 02:14:47 +0200

    removed explicit datatype float, instead now dispatches automatically with AT_DISPATCH_FLOATING_TYPES

--- src/cuda/GradientMap_cpu.cpp
+++ src/cuda/GradientMap_cpu.cpp
@@ -3,13 +3,13 @@
 // THE ACTUAL COMPUTATION LOOPS, AS MACROS, TO MAKE THE MAIN CODE MORE READABLE
 #define RECONSTRUCTION_LOOP_TL_TO_BR(code)                                                          \
 for(int color = 0; color < num_colors; color++){                                                    \
-    float* img_color_ptr = img_ptr + color * img_color_stride;                                      \
-    const float* grad_color_ptr = grad_ptr + color * grad_color_stride;                             \
+    scalar_t* img_color_ptr = img_ptr + color * img_color_stride;                                   \
+    const scalar_t* grad_color_ptr = grad_ptr + color * grad_color_stride;                          \
     for(int y = 1; y < height-1; y++){                                                              \
-        float* img_row_ptr = img_color_ptr + y * img_row_stride;                                    \
-        float* prev_img_row_ptr = img_color_ptr + (y-1) * img_row_stride;                           \
-        const float* grad_row_ptr = grad_color_ptr + y * grad_row_stride;                           \
-        const float* prev_grad_row_ptr = grad_color_ptr + (y-1) * grad_row_stride;                  \
+        scalar_t* img_row_ptr = img_color_ptr + y * img_row_stride;                                 \
+        scalar_t* prev_img_row_ptr = img_color_ptr + (y-1) * img_row_stride;                        \
+        const scalar_t* grad_row_ptr = grad_color_ptr + y * grad_row_stride;                        \
+        const scalar_t* prev_grad_row_ptr = grad_color_ptr + (y-1) * grad_row_stride;               \
         for(int x = 1; x < width-1; x++){                                                           \
             code                                                                                    \
         }                                                                                           \
@@ -17,12 +17,12 @@ for(int color = 0; color < num_colors; color++){                                
 }
 #define RECONSTRUCTION_LOOP_BR_TO_TL(code)                                                          \
 for(int color = num_colors-1; color >= 0; color--){                                                 \
-    float* img_color_ptr = img_ptr + color * img_color_stride;                                      \
-    const float* grad_color_ptr = grad_ptr + color * grad_color_stride;                             \
+    scalar_t* img_color_ptr = img_ptr + color * img_color_stride;                                   \
+    const scalar_t* grad_color_ptr = grad_ptr + color * grad_color_stride;                          \
     for(int y = height-2; y > 0; y--){                                                              \
-        float* img_row_ptr = img_color_ptr + y * img_row_stride;                                    \
-        float* prev_img_row_ptr = img_color_ptr + (y+1) * img_row_stride;                           \
-        const float* grad_row_ptr = grad_color_ptr + y * grad_row_stride;                           \
+        scalar_t* img_row_ptr = img_color_ptr + y * img_row_stride;                                 \
+        scalar_t* prev_img_row_ptr = img_color_ptr + (y+1) * img_row_stride;                        \
+        const scalar_t* grad_row_ptr = grad_color_ptr + y * grad_row_stride;                        \
         for(int x = width-2; x > 0; x--){                                                           \
             code                                                                                    \
         }                                                                                           \
@@ -31,10 +31,6 @@ for(int color = num_colors-1; color >= 0; color--){                             
 
 
 void step_cpu(int step, torch::Tensor img, torch::Tensor grad){
-
-    // Get raw pointers
-    float* img_ptr = img.data<float>();
-    const float* grad_ptr = grad.data<float>();
 
     // Pre-Compute some commonly used variables, for a little extra speed (works, measured)
     const int num_colors = img.size(0);
@@ -45,34 +41,41 @@ void step_cpu(int step, torch::Tensor img, torch::Tensor grad){
     const int grad_color_stride = grad.stride(0);
     const int grad_row_stride = grad.stride(1);
 
-    switch(step%4){
-        case 0: // Left to Right
-            RECONSTRUCTION_LOOP_TL_TO_BR({
-                // reference: img[col][y][x] = (img[col][y][x] + img[col][y][x - 1] + grad[col][y][x - 1]) / 2;
-                img_row_ptr[x] = (img_row_ptr[x] + img_row_ptr[x - 1] + grad_row_ptr[x - 1]) / 2;
-            })
-            break;
+    // Automatically determine the data type of our CPU kernels
+    AT_DISPATCH_FLOATING_TYPES(img.type(), "step_cpu", ([&] {
+        // Get raw pointers
+        scalar_t* img_ptr = img.data<scalar_t>();
+        const scalar_t* grad_ptr = grad.data<scalar_t>();
 
-        case 1: // Top to Bottom
-            RECONSTRUCTION_LOOP_TL_TO_BR({
-                // reference: img[col][y][x] = (img[col][y][x] + img[col][y-1][x] + grad[col][y-1][x]) / 2;
-                img_row_ptr[x] = (img_row_ptr[x] + prev_img_row_ptr[x] + prev_grad_row_ptr[x]) / 2;
-            })
-            break;
+        switch(step%4){
+            case 0: // Left to Right
+                RECONSTRUCTION_LOOP_TL_TO_BR({
+                    // reference: img[col][y][x] = (img[col][y][x] + img[col][y][x - 1] + grad[col][y][x - 1]) / 2;
+                    img_row_ptr[x] = (img_row_ptr[x] + img_row_ptr[x - 1] + grad_row_ptr[x - 1]) / 2;
+                })
+                break;
 
-        case 2:// Right to Left
-            RECONSTRUCTION_LOOP_BR_TO_TL({
-                // reference: img[col][y][x] = (img[col][y][x] + img[col][y][x + 1] - grad[col][y][x]) / 2;
-                img_row_ptr[x] = (img_row_ptr[x] + img_row_ptr[x+1] - grad_row_ptr[x]) / 2;
-            })
-            break;
+            case 1: // Top to Bottom
+                RECONSTRUCTION_LOOP_TL_TO_BR({
+                    // reference: img[col][y][x] = (img[col][y][x] + img[col][y-1][x] + grad[col][y-1][x]) / 2;
+                    img_row_ptr[x] = (img_row_ptr[x] + prev_img_row_ptr[x] + prev_grad_row_ptr[x]) / 2;
+                })
+                break;
 
-        default: // Bottom to Top
-            RECONSTRUCTION_LOOP_BR_TO_TL({
-                // reference: img[col][y][x] = (img[col][y][x] + img[col][y+1][x] - grad[col][y][x]) / 2;
-                img_row_ptr[x] = (img_row_ptr[x] + prev_img_row_ptr[x] - grad_row_ptr[x]) / 2;
-            })
-            break;
-    }
+            case 2:// Right to Left
+                RECONSTRUCTION_LOOP_BR_TO_TL({
+                    // reference: img[col][y][x] = (img[col][y][x] + img[col][y][x + 1] - grad[col][y][x]) / 2;
+                    img_row_ptr[x] = (img_row_ptr[x] + img_row_ptr[x+1] - grad_row_ptr[x]) / 2;
+                })
+                break;
+
+            default: // Bottom to Top
+                RECONSTRUCTION_LOOP_BR_TO_TL({
+                    // reference: img[col][y][x] = (img[col][y][x] + img[col][y+1][x] - grad[col][y][x]) / 2;
+                    img_row_ptr[x] = (img_row_ptr[x] + prev_img_row_ptr[x] - grad_row_ptr[x]) / 2;
+                })
+                break;
+        }
+    }));
 }
 
--- src/cuda/GradientMap_cuda.cu
+++ src/cuda/GradientMap_cuda.cu
@@ -10,14 +10,17 @@
 // https://devblogs.nvidia.com/even-easier-introduction-cuda/
 // The amazing part is that you can change the block and dimension size without having to adjust the kernel,
 // for performance comparison. It will automatically adjust and still compute all the necessary data.
-#define RECONSTRUCTION_FUNCTION(name, code) \
-__global__ void name ## _cuda(\
-    torch::PackedTensorAccessor<float,3,torch::RestrictPtrTraits,size_t> img,\
-    const torch::PackedTensorAccessor<float,3,torch::RestrictPtrTraits,size_t>  grad_x){\
-    int index = blockIdx.x * blockDim.x + threadIdx.x;\
-    int stride = blockDim.x * gridDim.x;\
-    int col = blockIdx.y;\
-    code}
+#define RECONSTRUCTION_FUNCTION(name, code)                                                         \
+template <typename scalar_t>                                                                        \
+__global__ void name ## _cuda(                                                                      \
+    torch::PackedTensorAccessor<scalar_t,3,torch::RestrictPtrTraits,size_t> img,                    \
+    const torch::PackedTensorAccessor<scalar_t,3,torch::RestrictPtrTraits,size_t>  grad_x)          \
+{                                                                                                   \
+    int index = blockIdx.x * blockDim.x + threadIdx.x;                                              \
+    int stride = blockDim.x * gridDim.x;                                                            \
+    int col = blockIdx.y;                                                                           \
+    code                                                                                            \
+}
 
 
 RECONSTRUCTION_FUNCTION(lr_kernel,{
@@ -66,23 +69,26 @@ void step_cuda(int step, torch::Tensor img, torch::Tensor grad){
     // Add a second dimension for the colors
     const dim3 numBlocksWithColors(numBlocks,img.size(0));
 
-    // Create accessors
-    auto img_accessor = img.packed_accessor<float,3,torch::RestrictPtrTraits,size_t>();
-    auto grad_accessor = grad.packed_accessor<float,3,torch::RestrictPtrTraits,size_t>();
+    // Automatically determine the data type of our CUDA kernels
+    AT_DISPATCH_FLOATING_TYPES(img.type(), "step_cuda", ([&] {
+        // Create accessors
+        auto img_accessor = img.packed_accessor<scalar_t,3,torch::RestrictPtrTraits,size_t>();
+        auto grad_accessor = grad.packed_accessor<scalar_t,3,torch::RestrictPtrTraits,size_t>();
 
-    // The actual step
-    switch(step%4){
-        case 0:
-            lr_kernel_cuda<<<numBlocksWithColors,blockSize>>>(img_accessor, grad_accessor);
-            break;
-        case 1:
-            tb_kernel_cuda<<<numBlocksWithColors,blockSize>>>(img_accessor, grad_accessor);
-            break;
-        case 2:
-            rl_kernel_cuda<<<numBlocksWithColors,blockSize>>>(img_accessor, grad_accessor);
-            break;
-        default:
-            bt_kernel_cuda<<<numBlocksWithColors,blockSize>>>(img_accessor, grad_accessor);
-            break;
-    }
+        // The actual step
+        switch(step%4){
+            case 0:
+                lr_kernel_cuda<scalar_t><<<numBlocksWithColors,blockSize>>>(img_accessor, grad_accessor);
+                break;
+            case 1:
+                tb_kernel_cuda<scalar_t><<<numBlocksWithColors,blockSize>>>(img_accessor, grad_accessor);
+                break;
+            case 2:
+                rl_kernel_cuda<scalar_t><<<numBlocksWithColors,blockSize>>>(img_accessor, grad_accessor);
+                break;
+            default:
+                bt_kernel_cuda<scalar_t><<<numBlocksWithColors,blockSize>>>(img_accessor, grad_accessor);
+                break;
+        }
+    }));
 }
